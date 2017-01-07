11d5a24161f222c29a6fe56d55adf094cc665707
Itai <Itai@Itai-PC>
2017-01-07 15:58:17 +0200

    book page

---
+++ client/src/control/BookPageController.java
@@ -0,0 +1,34 @@
+package control;
+
+import java.io.IOException;
+
+import enums.ActionType;
+import interfaces.ScreensIF;
+import javafx.event.ActionEvent;
+
+/** BookPageController. Responsible to show a specific book page.
+ * @author itain
+ */
+
+
+public class BookPageController implements ScreensIF{
+
+	@Override
+	public void backButtonPressed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pressedCloseMenu(ActionEvent event) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionOnError(ActionType type, String errorCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
