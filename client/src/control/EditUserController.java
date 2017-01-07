c0eff705ade0f4704e830783bbb069f05531c44f
Itai <Itai@Itai-PC>
2017-01-07 18:22:04 +0200

    user search results + edit users

---
+++ client/src/control/EditUserController.java
@@ -0,0 +1,34 @@
+package control;
+
+import java.io.IOException;
+
+import enums.ActionType;
+import interfaces.ScreensIF;
+import javafx.event.ActionEvent;
+
+/** EditUserController. Responsible to enable a librarian or a manager to edit user details.
+ * @author itain
+ */
+
+
+public class EditUserController implements ScreensIF{
+
+	@Override
+	public void backButtonPressed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pressedCloseMenu(ActionEvent event) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionOnError(ActionType type, String errorCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ client/src/control/SearchUserResultsController.java
@@ -0,0 +1,36 @@
+package control;
+
+import java.io.IOException;
+
+import enums.ActionType;
+import interfaces.ScreensIF;
+import javafx.event.ActionEvent;
+
+
+
+/** SearchUserResultsController. Responsible to show user search results and do actions on them.
+ * @author itain
+ */
+
+
+public class SearchUserResultsController implements ScreensIF{
+
+	@Override
+	public void backButtonPressed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pressedCloseMenu(ActionEvent event) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionOnError(ActionType type, String errorCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
