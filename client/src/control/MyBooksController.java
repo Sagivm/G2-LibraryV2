c7e2c49182bcbcaa412b57f4c397f560c4fb3be9
Itai <Itai@Itai-PC>
2017-01-07 16:28:41 +0200

    my books + search user

---
+++ client/src/control/MyBooksController.java
@@ -0,0 +1,33 @@
+package control;
+
+import java.io.IOException;
+
+import enums.ActionType;
+import interfaces.ScreensIF;
+import javafx.event.ActionEvent;
+
+/** MyBooksController. Responsible to show a user his purchased books.
+ * @author itain
+ */
+
+public class MyBooksController implements ScreensIF{
+
+	@Override
+	public void backButtonPressed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pressedCloseMenu(ActionEvent event) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionOnError(ActionType type, String errorCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ client/src/control/UserSearchController.java
@@ -0,0 +1,35 @@
+package control;
+
+import java.io.IOException;
+
+import enums.ActionType;
+import interfaces.ScreensIF;
+import javafx.event.ActionEvent;
+
+
+/** UserSearchController. Responsible to enable a librarian or a manager to search users.
+ * @author itain
+ */
+
+
+public class UserSearchController implements ScreensIF{
+
+	@Override
+	public void backButtonPressed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pressedCloseMenu(ActionEvent event) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionOnError(ActionType type, String errorCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
